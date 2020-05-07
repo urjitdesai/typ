c21d6effdf3a5725975d3499012c9184e92cc9bb
urjitdesai <urjitdesai1998@gmail.com>
2020-05-07 17:04:51 +0530

    Updates

---
+++ precache-manifest.2721658d4f0c4d65107c33c59243ee51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de1a6b1aaa06999e7da5b07143e659fa",
+    "url": "/typ/index.html"
+  },
+  {
+    "revision": "7cff0931aabf5740f4f3",
+    "url": "/typ/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "783c53c35577f859cb6f",
+    "url": "/typ/static/css/main.ad70b6bc.chunk.css"
+  },
+  {
+    "revision": "7cff0931aabf5740f4f3",
+    "url": "/typ/static/js/2.14a4b9d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/typ/static/js/2.14a4b9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783c53c35577f859cb6f",
+    "url": "/typ/static/js/main.058f7203.chunk.js"
+  },
+  {
+    "revision": "f595db93b19ef0746479",
+    "url": "/typ/static/js/runtime-main.68ddf384.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/typ/static/media/logo.9f7e506d.svg"
+  }
+]);
